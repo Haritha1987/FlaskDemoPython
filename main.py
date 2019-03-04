28b2887ddc059218c99da386997aca649b05d324
Haritha1987 <34791218+Haritha1987@users.noreply.github.com>
2019-03-04 18:19:52 +0530

    Updated msg

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hey its Python Flask application!'
+  return 'Now the python is working! :D'
 
 if __name__ == '__main__':
   app.run()
